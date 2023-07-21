b46f7cc21081e4f188337f253818ec1be625e509
Abraxas-365 <luisfmiranda8@gmail.com>
2023-07-21 17:33:52 -0500

    adding display to apiError

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -8,3 +8,12 @@ pub enum ApiError {
     OpenaiError(OpenaiError),
     AWSError(AWSError),
 }
+
+impl std::fmt::Display for ApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApiError::OpenaiError(err) => write!(f, "OpenAI error: {}", err),
+            ApiError::AWSError(err) => write!(f, "AWS error: {}", err),
+        }
+    }
+}
