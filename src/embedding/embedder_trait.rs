6633554930891ce157b105e16dc16d12fbdf32b9
Abraxas-365 <luisfmiranda8@gmail.com>
2023-07-21 16:20:54 -0500

    adding send

--- src/embedding/embedder_trait.rs
+++ src/embedding/embedder_trait.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use crate::errors::ApiError;
 
 #[async_trait]
-pub trait Embedder {
+pub trait Embedder: Send {
     async fn embed_documents(&self, documents: Vec<String>) -> Result<Vec<Vec<f64>>, ApiError>;
     async fn embed_query(&self, text: &str) -> Result<Vec<f64>, ApiError>;
 }
