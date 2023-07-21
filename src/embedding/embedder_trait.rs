fba754cab86215a88273804a9b58ae90897b1b11
Abraxas-365 <luisfmiranda8@gmail.com>
2023-07-21 16:38:51 -0500

    adding sync

--- src/embedding/embedder_trait.rs
+++ src/embedding/embedder_trait.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use crate::errors::ApiError;
 
 #[async_trait]
-pub trait Embedder: Send {
+pub trait Embedder: Send + Sync {
     async fn embed_documents(&self, documents: Vec<String>) -> Result<Vec<Vec<f64>>, ApiError>;
     async fn embed_query(&self, text: &str) -> Result<Vec<f64>, ApiError>;
 }
