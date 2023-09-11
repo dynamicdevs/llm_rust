32eba9235656ee77c0ec5ce7abc5998b49a19e80
Abraxas-365 <luisfmiranda8@gmail.com>
2023-09-11 15:51:05 -0500

    adding gpt4 support

--- src/chat_models/openai/chat_llm.rs
+++ src/chat_models/openai/chat_llm.rs
@@ -19,12 +19,14 @@ use crate::{
 pub enum ChatModel {
     Gpt3_5Turbo,
     Gpt3_5Turbo16k,
+    Gpt4,
 }
 impl ChatModel {
     pub fn as_str(&self) -> &str {
         match *self {
             ChatModel::Gpt3_5Turbo => "gpt-3.5-turbo",
             ChatModel::Gpt3_5Turbo16k => "gpt-3.5-turbo-16k",
+            ChatModel::Gpt4 => "gpt-4",
         }
     }
 }
