5cd72c862006a574835af5e33178a7d0127c7969
Abraxas-365 <luisfmiranda8@gmail.com>
2023-10-07 16:45:01 -0500

    feat(prompt): editing prompt

--- src/agents/chat/prompt.rs
+++ src/agents/chat/prompt.rs
@@ -28,7 +28,7 @@ Use this if you want to respond directly to the human. Markdown code snippet for
 ```json
 {
     "action": "Final Answer",
-    "action_input": string \\ You should put what you want to return to use here.Bold for Emphasis names,important information. Lists for organization. Tables for structuring data and when need to compare products.
+    "action_input": string \\ You should put what you want to return to use here
 }
 ```"#;
 
