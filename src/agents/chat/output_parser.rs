57c359427a0dff918aee08cd843325132f456e1e
Abraxas-365 <luisfmiranda8@gmail.com>
2023-10-17 14:14:10 -0500

    feat(parser): changin regex

--- src/agents/chat/output_parser.rs
+++ src/agents/chat/output_parser.rs
@@ -20,10 +20,6 @@ impl ConvoOutputParser {
     pub fn new() -> Self {
         Self {}
     }
-
-    fn clean_json_match(&self, matched: &str) -> String {
-        matched.replace("\\\"", "\"")
-    }
 }
 
 impl AgentOutputParser for ConvoOutputParser {
@@ -34,15 +30,11 @@ impl AgentOutputParser for ConvoOutputParser {
             .collect::<String>();
 
         log::debug!("Parsing to Agent Action: {}", sanitized_text);
-        let re = Regex::new(r"```(json)?(.*?)```")?;
+        let re = Regex::new(r"```json?\s*(.*?)\s*```").unwrap();
         let json_match = re.captures(&sanitized_text).and_then(|cap| cap.get(1));
         log::debug!("Finish extracting json");
         let agent_output: AgentOutput = match json_match {
-            Some(json_str) => {
-                let cleaned_str = self.clean_json_match(json_str.as_str());
-                log::debug!("Cleaned Json:{:?}", cleaned_str);
-                serde_json::from_str(&cleaned_str)?
-            }
+            Some(json_str) => serde_json::from_str(&json_str.as_str())?,
             None => {
                 log::debug!("No JSON found in text: {}", sanitized_text);
                 return Ok(AgentEvent::Finish(AgentFinish {
@@ -67,4 +59,53 @@ impl AgentOutputParser for ConvoOutputParser {
     fn get_format_instructions(&self) -> &str {
         FORMAT_INSTRUCTIONS
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_json_within_backticks() {
+        let parser = ConvoOutputParser::new();
+        let input = r#"```json
+    {
+        "action": "Product_information",
+        "action_input": "{\"products\":[\"Laptop Lenovo LOQ 15IRH8 15.6 Core i7-13620H 16G 512 SSD V6G\"],\"query\":\"Dame mas info\"}"
+    }
+    ```"#;
+
+        match parser.parse(input) {
+            Ok(AgentEvent::Action(action)) => {
+                println!("Action Tool Input: {}", action.tool_input);
+                assert_eq!(action.tool, "Product_information");
+                // Corrected the expected value of `tool_input` for the assertion
+                assert_eq!(action.tool_input, "{\"products\":[\"Laptop Lenovo LOQ 15IRH8 15.6 Core i7-13620H 16G 512 SSD V6G\"],\"query\":\"Dame mas info\"}");
+            }
+            Ok(_result) => {
+                // Print the unexpected result for diagnostic purposes
+                panic!("Expected an AgentAction but got something else.");
+            }
+            Err(e) => {
+                // Print the error for diagnostic purposes
+                println!("Error: {}", e);
+                panic!("Error occurred during parsing.");
+            }
+        }
+    }
+
+    #[test]
+    fn test_no_json_within_backticks() {
+        let parser = ConvoOutputParser::new();
+        let input = r"some text without any json backticks";
+
+        match parser.parse(input) {
+            Ok(AgentEvent::Finish(finish)) => {
+                assert_eq!(finish.return_values, input);
+            }
+            _ => panic!("Expected an AgentFinish but got something else."),
+        }
+    }
+
+    // You can add more tests as required
 }
